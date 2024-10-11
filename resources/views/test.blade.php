5ccb6eeffe6bdc29ba6481dca66b2cb401005870
Bilal Khilji <kbinfo4u@gmail.com>
2024-10-11 12:50:33 +0530

    Events

---
+++ resources/views/test.blade.php
@@ -0,0 +1,6 @@
+<script>
+    Echo.channel('reverb-test')
+        .listen('ReverbTest', (e) => {
+            console.log(e)
+        })
+</script>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Events\ReverbTest;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('test');
+});
+
+Route::get('send', function () {
+    ReverbTest::dispatch();
 });
